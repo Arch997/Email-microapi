c04277c94354e668b8211cedb575416bbd1d4925
Arch997 <archibongc4@gmail.com>
2020-06-21 01:48:20 +0100

    Wrote Password Reset serializer class

--- api/serializers.py
+++ api/serializers.py
@@ -18,3 +18,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+
+class PasswordResetSerializer(serializers.ModelSerializer):
+    """Serializer class for the Password Reset API View."""
+    email = serializers.EmailField(source='User.email')
+    password = serializers.CharField(source='User.password', style={'input_type': 'password'})
+    new_password = serializers.CharField(source='User.password', style={'input_type': 'password'})
+    
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'new_password')
+
+    def update(self, instance, validated_data):
+        # instance.username = validated_data.get('username', instance.username)
+        # instance.email = validated_data.get('email', instance.email)
+        instance.password = make_password(instance.password)
+        instance.password = validated_data.get('password', instance.password)
+        instance.save()
+        return instance
