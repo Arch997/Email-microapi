ec766456f1b482c246c6d77f1fa84e29ea5bdd9b
Arch997 <archibongc4@gmail.com>
2020-06-21 01:22:48 +0100

    Added Password Reset Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
+from django.contrib.auth import password_validation as validators
+from django.contrib.auth.hashers import make_password
 
 class MailSerializer(serializers.Serializer):
     recipient = serializers.EmailField()
@@ -18,3 +20,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+        
+
+class PasswordResetSerializer(serializers.ModelSerializer):
+    """Serializer class for the Password Reset API View."""
+    email = serializers.EmailField(source='User.email')
+    password = serializers.CharField(source='User.password', style={'input_type': 'password'})
+    new_password = serializers.CharField(source='User.password', style={'input_type': 'password'})
+    
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'new_password')
+
+    def update(self, instance, validated_data):
+        # instance.username = validated_data.get('username', instance.username)
+        # instance.email = validated_data.get('email', instance.email)
+        instance.password = make_password(instance.password)
+        instance.password = validated_data.get('password', instance.password)
+        instance.save()
+        return instance
