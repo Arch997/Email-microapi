1c98159158bf857398ec1b14a427685314178f39
Arch997 <archibongc4@gmail.com>
2020-06-21 01:23:41 +0100

    Added password reset url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('sendmail', views.SendMail.as_view()),
     path('sendmailwithtemplate', views.SendMailWithTemplate.as_view()),
     path('register', views.UserCreate.as_view(), name='account-create'),
-    path('sendscheduledmail', views.SendScheduledMail.as_view())
+    path('sendscheduledmail', views.SendScheduledMail.as_view()),
+	path('resetpassword', views.PasswordReset.as_view(), name='reset_password'),
+	
 ]
